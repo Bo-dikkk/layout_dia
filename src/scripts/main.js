930787d9784db62cc3894f7cb01fc9a7d8cf9a76
Bohdan Krochak <krochak2002714@gmail.com>
2026-05-05 17:50:21 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,2 @@
 'use strict';
 
-const slides = [
-  'src/images/slider/slide-img-1.jpg',
-  'src/images/slider/slide-img-2.jpg',
-  'src/images/slider/slide-img-3.jpg'
-];
-let currentSlide = 0;
-
-const slider = document.querySelector('.slide-bar');
-const prevBtn = document.querySelector('.slide-bar__button--left');
-const nextBtn = document.querySelector('.slide-bar__button--right');
-
-nextBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  currentSlide = (currentSlide + 1) % slides.length;
-  slider.style.backgroundImage = `url(${slides[currentSlide]})`;
-});
-prevBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  slider.style.backgroundImage = `url(${slides[currentSlide]})`;
-});
-
